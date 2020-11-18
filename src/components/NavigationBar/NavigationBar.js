2788ef9cfc3eb72b8de49fdacf1d02fec0eb8a1d
tonplamm <tonplam_kanokpol@hotmail.com>
2020-11-18 15:32:45 +0700

    feaat: navigation bar component

--- src/components/NavigationBar/NavigationBar.js
+++ src/components/NavigationBar/NavigationBar.js
@@ -14,8 +14,12 @@ class NavigationBar extends React.Component {
     
     render() {
         return (
-            <div className="NavigationBar">
-                NavigationBar
+            <div className="NavigationBarContainer">
+                <div className="NavigateBreadCrumb flex justify-start px-4 md:px-8 py-3 z-10 overflow-x-auto">
+                    <span className="Underlined">หน้าแรก</span>
+                    <span className="Seperator">/</span>
+                    <span className="font-bold">ค้นหา</span>
+                </div>
             </div>
         )
     }
